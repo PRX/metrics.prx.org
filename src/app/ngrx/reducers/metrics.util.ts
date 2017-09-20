21538b55189aed8ba3813715b83171a01066525c
Sandi Barr <sandi.barr@prx.org>
2017-09-20 15:21:13 -0500

    prevent array access errors in subtract util

--- src/app/ngrx/reducers/metrics.util.ts
+++ src/app/ngrx/reducers/metrics.util.ts
@@ -4,7 +4,9 @@ export const subtractDataset = (from: any[][], dataset: any[][]) => {
   // may want to reconsider this in the future if we need to cache extra data outside the filter to support interactive zooming on the chart
   const subtracted = [];
   for (let i = 0; i < from.length; i++) {
-    subtracted.push([from[i][0], from[i][1] - dataset[i][1]]);
+    if (dataset[i]) {
+      subtracted.push([from[i][0], from[i][1] - dataset[i][1]]);
+    }
   }
   return subtracted;
 };

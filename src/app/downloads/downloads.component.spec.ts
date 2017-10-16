a544a78f937f7b3911a5795b2007e917993cdb92
gcampo88 <gigi.campo@gmail.com>
2017-10-16 12:06:56 -0400

    nix dup id

--- src/app/downloads/downloads.component.spec.ts
+++ src/app/downloads/downloads.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';

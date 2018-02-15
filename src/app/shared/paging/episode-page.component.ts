292b4206b2fd0d718df5d78b15cc00350b07394e
Peter Karman <peter.karman@prx.org>
2018-02-15 12:29:51 -0600

    refactor paging logic to use only page numbers rather than array indices

--- src/app/shared/paging/episode-page.component.ts
+++ src/app/shared/paging/episode-page.component.ts
@@ -4,15 +4,24 @@ import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core
   selector: 'metrics-episode-page',
   template: `
     <div *ngIf="currentPage && totalPages" class="pager">
-      <button [disabled]="prevDisabled" (click)="pageChange.emit(1)" title="Page 1" class="btn-link">|&#171;</button>
-      <button [disabled]="prevDisabled" title="Page {{currentPage - 1}}" (click)="pageChange.emit(currentPage - 1)" class="btn-link">&#171;</button>
-      <button *ngFor="let page of pages | slice:pagesBegin:pagesEnd;"
+      <button [disabled]="prevDisabled" (click)="pageChange.emit(1)" title="Page 1" class="pager">|&#171;</button>
+      <button [disabled]="prevDisabled"
+              (click)="pageChange.emit(currentPage - 1)"
+              title="Page {{currentPage == 1 ? 1 : currentPage - 1}}"
+              class="pager">&#171;</button>
+      <button *ngFor="let page of pages"
               [disabled]="page === currentPage" [class.active]="page === currentPage"
               (click)="pageChange.emit(page)"
-              class="btn-link">{{page}}</button>
-      <button disabled *ngIf="pages.length > showNumPages" class="btn-link">of {{pages.length}}</button>
-      <button [disabled]="nextDisabled" (click)="pageChange.emit(currentPage + 1)" title="Page {{currentPage + 1}}" class="btn-link">&#187;</button>
-      <button [disabled]="nextDisabled" (click)="pageChange.emit(lastPage)" title="Page {{lastPage}}" class="btn-link">&#187;|</button>
+              class="pager">{{page}}</button>
+      <button disabled *ngIf="lastPage > showNumPages" class="pager">of {{lastPage}}</button>
+      <button [disabled]="nextDisabled"
+              (click)="pageChange.emit(currentPage + 1)"
+              title="Page {{currentPage + 1}}"
+              class="pager">&#187;</button>
+      <button [disabled]="nextDisabled"
+              (click)="pageChange.emit(lastPage)"
+              title="Page {{lastPage}}"
+              class="pager">&#187;|</button>
     </div>
   `,
   styleUrls: ['episode-page.component.css']
@@ -28,30 +37,31 @@ export class EpisodePageComponent implements OnChanges {
   lastPage: number;
 
   ngOnChanges() {
-    this.pages = [];
-    for (let i = 1; i <= this.totalPages; i++) {
-      this.pages.push(i);
-    }
     this.lastPage = Math.floor(this.totalPages);
 
     if (this.totalPages <= this.showNumPages) {
-      this.pagesBegin = 0;
-      this.pagesEnd = this.lastPage - 1;
+      this.pagesBegin = 1;
+      this.pagesEnd = this.lastPage;
     }
     else {
       var halfWindow = Math.floor(this.showNumPages / 2);
       if (this.currentPage <= (halfWindow + 1)) {
-        this.pagesBegin = 0;
+        this.pagesBegin = 1;
         this.pagesEnd = this.showNumPages;
       }
-      else if ((this.currentPage + (halfWindow - 1)) >= this.totalPages) {
-        this.pagesBegin = this.totalPages - this.showNumPages;
-        this.pagesEnd = this.totalPages;
+      else if ((this.currentPage + halfWindow) >= this.lastPage) {
+        this.pagesBegin = this.lastPage - this.showNumPages + 1;
+        this.pagesEnd = this.lastPage;
       }
       else {
         this.pagesBegin = this.currentPage - halfWindow;
         this.pagesEnd = this.currentPage + halfWindow;
       }
+    }
+
+    this.pages = [];
+    for (let i = this.pagesBegin; i <= this.pagesEnd; i++) {
+      this.pages.push(i);
     }
   }
 
@@ -60,6 +70,6 @@ export class EpisodePageComponent implements OnChanges {
   }
 
   get nextDisabled(): boolean {
-    return this.currentPage === this.pages.length;
+    return this.currentPage === this.lastPage;
   }
 }

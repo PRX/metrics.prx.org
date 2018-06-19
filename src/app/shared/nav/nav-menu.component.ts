f5f5486124150669903287f06fea3cb9a100109e
Sandi Barr <sandi.barr@prx.org>
2018-06-19 17:49:27 -0500

    responsive side nav

--- src/app/shared/nav/nav-menu.component.ts
+++ src/app/shared/nav/nav-menu.component.ts
@@ -8,17 +8,26 @@ import * as ACTIONS from '../../ngrx/actions';
 @Component({
   selector: 'metrics-nav-menu',
   template: `
-    <nav>
-      <button *ngFor="let t of types"
-          (click)="routeMetricsType(t.type)"
-         [class.active]="routerState.metricsType === t.type">
-        <span
-          [class]="t.icon"
-          [class.grey-darkest]="routerState.metricsType !== t.type"
-          [class.active]="routerState.metricsType === t.type" aria-hidden="true"></span>
-        <span class="text">{{t.name}}</span>
-      </button>
-    </nav>
+    <div class="dropdown" [class.open]="open">
+      <div class="dropdown-button">
+        <button (click)="toggleOpen()" >{{selectedNav?.name}}<span class="down-arrow"></span></button>
+      </div>
+      <div class="dropdown-content rollout">
+        <nav>
+          <li *ngFor="let t of types">
+            <button
+              (click)="routeMetricsType(t.type)"
+              [class.active]="routerState.metricsType === t.type">
+              <span
+                [class]="t.icon"
+                [class.grey-darkest]="routerState.metricsType !== t.type"
+                [class.active]="routerState.metricsType === t.type" aria-hidden="true"></span>
+              <span class="text">{{t.name}}</span>
+            </button>
+          </li>
+        </nav>
+      </div>
+    </div>
   `,
   styleUrls: ['nav-menu.component.css']
 })
@@ -30,6 +39,7 @@ export class NavMenuComponent implements OnDestroy {
     {name: 'Demographics', type: METRICSTYPE_DEMOGRAPHICS, icon: 'icon icon-globe'},
     {name: 'Devices', type: METRICSTYPE_TRAFFICSOURCES, icon: 'icon icon-smartphone'}
   ];
+  open = false;
 
   constructor(public store: Store<any>) {
     this.routerSub = this.store.pipe(select(selectRouter)).subscribe((routerState) => this.routerState = routerState);
@@ -41,5 +51,13 @@ export class NavMenuComponent implements OnDestroy {
 
   routeMetricsType(metricsType: MetricsType) {
     this.store.dispatch(new ACTIONS.RouteMetricsTypeAction({metricsType}));
+  }
+
+  get selectedNav() {
+    return this.routerState && this.types.find(type => type.type === this.routerState.metricsType);
+  }
+
+  toggleOpen() {
+    this.open = !this.open;
   }
 }

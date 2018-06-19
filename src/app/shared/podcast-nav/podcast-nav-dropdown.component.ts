bab2688887738b26a498d383e3380566a332cf62
Sandi Barr <sandi.barr@prx.org>
2018-06-19 11:56:35 -0500

    podcast nav uses display font

--- src/app/shared/podcast-nav/podcast-nav-dropdown.component.ts
+++ src/app/shared/podcast-nav/podcast-nav-dropdown.component.ts
@@ -17,7 +17,7 @@ import { PodcastModel } from '../../ngrx';
         </metrics-podcast-nav-list>
       </div>
     </div>
-    <span *ngIf="podcasts?.length === 1 && selectedPodcast">{{ selectedPodcast.title }}</span>
+    <span class="single" *ngIf="podcasts?.length === 1 && selectedPodcast">{{ selectedPodcast.title }}</span>
   `,
   styleUrls: ['../menu/dropdown.css', './podcast-nav-dropdown.component.css']
 })

69a8373c22ae267e9940e68a72e5879ebd91d362
Sandi Barr <sandi.barr@prx.org>
2018-07-18 18:38:21 -0500

    sorts podcast/show list

--- src/app/ngrx/reducers/selectors/podcast.selectors.ts
+++ src/app/ngrx/reducers/selectors/podcast.selectors.ts
@@ -6,7 +6,11 @@ import { selectPodcastRoute } from './router.selectors';
 export const selectPodcastState = createSelector(selectAppState, (state: RootState) => state.podcasts);
 export const selectPodcastEntities = createSelector(selectPodcastState, getPodcastEntities);
 export const selectPodcasts = createSelector(selectPodcastEntities, entities => {
-  return Object.keys(entities).map(seriesId => entities[parseInt(seriesId, 10)]);
+  return Object.keys(entities)
+    .sort((seriesIdA, seriesIdB) => {
+      return entities[parseInt(seriesIdA, 10)].title.localeCompare(entities[parseInt(seriesIdB, 10)].title);
+    })
+    .map(seriesId => entities[parseInt(seriesId, 10)]);
 });
 export const selectPodcastsLoaded = createSelector(selectPodcastState, getPodcastsLoaded);
 export const selectPodcastsLoading = createSelector(selectPodcastState, getPodcastsLoading);

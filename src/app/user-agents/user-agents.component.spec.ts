df2bfab2faa4d6580d2d0250766d78c70829cc63
Sandi Barr <sandi.barr@prx.org>
2018-08-29 16:13:57 -0500

    user agents components

--- src/app/user-agents/user-agents.component.spec.ts
+++ src/app/user-agents/user-agents.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, Store } from '@ngrx/store';
+import { FancyFormModule } from 'ngx-prx-styleguide';
 
 import { SharedModule } from '../shared/shared.module';
+import * as ACTIONS from '../ngrx/actions';
 import { reducers } from '../ngrx/reducers';
+import { MetricsType, GroupType, GROUPTYPE_AGENTNAME, METRICSTYPE_TRAFFICSOURCES } from '../ngrx/reducers/models';
 
 import { UserAgentsComponent } from './user-agents.component';
+import { UserAgentsChartComponent } from './user-agents-chart.component';
+import { podcast, routerParams as downloadParams, podcastAgentNameRanks, podcastAgentNameDownloads } from '../../testing/downloads.fixtures';
 
 describe('UserAgentsComponent', () => {
+  let store: Store<any>;
   let comp: UserAgentsComponent;
   let fix: ComponentFixture<UserAgentsComponent>;
   let de: DebugElement;
@@ -17,10 +24,12 @@ describe('UserAgentsComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        UserAgentsComponent
+        UserAgentsComponent,
+        UserAgentsChartComponent
       ],
       imports: [
         SharedModule,
+        FancyFormModule,
         StoreModule.forRoot(reducers),
         RouterTestingModule
       ]
@@ -30,10 +39,54 @@ describe('UserAgentsComponent', () => {
       fix.detectChanges();
       de = fix.debugElement;
       el = de.nativeElement;
+      store = TestBed.get(Store);
     });
   }));
+
+  const routerParams = {...downloadParams, metricsType:<MetricsType>METRICSTYPE_TRAFFICSOURCES, group: <GroupType>GROUPTYPE_AGENTNAME};
+  function dispatchRouterNavigation() {
+    store.dispatch(new ACTIONS.CustomRouterNavigationAction({routerParams}));
+  }
+
+  function dispatchPodcasts() {
+    store.dispatch(new ACTIONS.CastlePodcastPageSuccessAction(
+      {podcasts: [podcast], page: 1, total: 1}));
+  }
+
+  function dispatchPodcastAgentNameTotals() {
+    store.dispatch(new ACTIONS.CastlePodcastTotalsSuccessAction({
+      id: routerParams.podcastId,
+      group: GROUPTYPE_AGENTNAME,
+      ranks: podcastAgentNameRanks
+    }));
+  }
+
+  function dispatchPodcastAgentNameRanks() {
+    store.dispatch(new ACTIONS.CastlePodcastRanksSuccessAction({
+      id: routerParams.podcastId,
+      group: GROUPTYPE_AGENTNAME,
+      interval: routerParams.interval,
+      ranks: podcastAgentNameRanks,
+      downloads: podcastAgentNameDownloads
+    }));
+  }
 
   it('should create the component', async(() => {
     expect(comp).toBeTruthy();
   }));
+
+  it('should show loading spinner when loading', () => {
+    fix.detectChanges();
+    expect(de.query(By.css('prx-spinner'))).not.toBeNull();
+  });
+
+  it('should show a totals table', () => {
+    dispatchPodcasts();
+    dispatchRouterNavigation();
+    dispatchPodcastAgentNameTotals();
+    dispatchPodcastAgentNameRanks();
+    fix.detectChanges();
+    expect(de.query(By.css('prx-spinner'))).toBeNull();
+    expect(de.query(By.css('metrics-totals-table'))).not.toBeNull();
+  });
 });

64b4fb2d48ad410a20b7585e71d3ef2b676c343a
Sandi Barr <sandi.barr@prx.org>
2018-09-25 13:17:51 -0500

    detect initial and changes in window size to set dynamic size of geochart map and redraw

--- src/app/geo/geochart.map.component.ts
+++ src/app/geo/geochart.map.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input, OnInit, OnChanges, SimpleChanges, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Env } from '../core/core.env';
 import { GROUPTYPE_GEOCOUNTRY, GROUPTYPE_GEOMETRO, RouterParams } from '../ngrx/index';
+import { fromEvent } from 'rxjs/observable/fromEvent';
+import { map } from 'rxjs/operators/map';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 declare const google: any;
 
@@ -9,12 +12,13 @@ declare const google: any;
   template: `<div #geo></div>`
 })
 
-export class GeochartMapComponent implements OnInit, OnChanges {
+export class GeochartMapComponent implements OnInit, OnChanges, AfterViewInit {
   private static googleLoaded: boolean;
   @ViewChild('geo') el: ElementRef;
   @Input() routerParams: RouterParams;
   @Input() data: any[][];
   @Input() nestedData: any[][];
+  windowSize: {width: number, height: number};
   colors = ['#e5f5fb', '#a6cee3', '#01a0dc', '#008fc5', '#0089bd', '#1f78b4'];
 
   ngOnInit() {
@@ -29,36 +33,61 @@ export class GeochartMapComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if (GeochartMapComponent.googleLoaded && google.visualization && this.data && changes.data) {
+    if (changes.data) {
       this.drawMap();
     }
   }
 
+  ngAfterViewInit() {
+    this.windowSize = {width: window.innerWidth, height: window.innerHeight};
+    this.drawMap();
+    fromEvent(window, 'resize').pipe(
+      map((event: Event) => {
+        this.windowSize = {width: event.target['innerWidth'], height: event.target['innerHeight']};
+        return this.windowSize;
+      }),
+      distinctUntilChanged()).subscribe(() => this.drawMap());
+  }
+
   drawMap() {
-    const options = {
-      displayMode: 'region',
-      colorAxis: {colors: this.colors},
-      legend: 'none',
-      height: 320
-    };
+    if (GeochartMapComponent.googleLoaded && google.visualization && this.data) {
+      let width, height;
+      if (this.windowSize && this.windowSize.width && this.windowSize.width <= 768) {
+        width = this.windowSize.width - 40;
+      } else {
+        height = 320;
+      }
+      const options = {
+        displayMode: 'region',
+        colorAxis: {colors: this.colors},
+        legend: 'none'
+      };
+      if (width) {
+        options['width'] = width;
+      }
+      if (height) {
+        options['height'] = height;
+      }
 
-    let data;
-    /* TODO: other regions are not working yet, need to use region codes and format rows for displaying name,
-    see https://developers.google.com/chart/interactive/docs/reference#DataTable
-    if (this.routerParams.group === GROUPTYPE_GEOCOUNTRY && this.routerParams.filter && this.nestedData) {
-      options['region'] = this.routerParams.filter;
-      options['resolution'] = 'provinces';
-      data = google.visualization.arrayToDataTable(this.nestedData);
-    } else */if (this.routerParams.group === GROUPTYPE_GEOCOUNTRY) {
-      options['region'] = 'world';
-      data = google.visualization.arrayToDataTable(this.data);
-    } else if (this.routerParams.group === GROUPTYPE_GEOMETRO) {
-      options['region'] = 'US';
-      options['resolution'] = 'metros';
-      data = google.visualization.arrayToDataTable(this.data);
+      let data;
+      /* TODO: other regions are not working yet, need to use region codes and format rows for displaying name,
+      see https://developers.google.com/chart/interactive/docs/reference#DataTable
+      if (this.routerParams.group === GROUPTYPE_GEOCOUNTRY && this.routerParams.filter && this.nestedData) {
+        options['region'] = this.routerParams.filter;
+        options['resolution'] = 'provinces';
+        data = google.visualization.arrayToDataTable(this.nestedData);
+      } else */
+      if (this.routerParams.group === GROUPTYPE_GEOCOUNTRY) {
+        options['region'] = 'world';
+        data = google.visualization.arrayToDataTable(this.data);
+      } else if (this.routerParams.group === GROUPTYPE_GEOMETRO) {
+        options['region'] = 'US';
+        options['resolution'] = 'metros';
+        data = google.visualization.arrayToDataTable(this.data);
+      }
+
+      const chart = new google.visualization.GeoChart(this.el.nativeElement);
+      chart.draw(data, options);
     }
-
-    const chart = new google.visualization.GeoChart(this.el.nativeElement);
-    chart.draw(data, options);
   }
 }

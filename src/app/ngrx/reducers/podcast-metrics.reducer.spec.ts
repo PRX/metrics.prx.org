b3dfc6ad4a2cf92ffd89404df59d8ee5bd164b5b
Peter St.Jean <peter@stjean.co>
2018-12-31 17:06:07 -0600

    Tests and entity implementation for loading and error metrics

--- src/app/ngrx/reducers/podcast-metrics.reducer.spec.ts
+++ src/app/ngrx/reducers/podcast-metrics.reducer.spec.ts
@@ -1,6 +1,6 @@
-import { CastlePodcastMetricsSuccessAction } from '../actions/castle.action.creator';
+import { CastlePodcastMetricsSuccessAction, CastlePodcastMetricsFailureAction, CastlePodcastMetricsLoadAction } from '../actions/castle.action.creator';
 import { RouterParams, INTERVAL_DAILY, MetricsType, METRICSTYPE_DOWNLOADS, getMetricsProperty } from './models';
-import { PodcastMetricsReducer, selectAllPodcastMetrics, selectPodcastMetricsIds, initialState as podcastMetricsInitialState } from './podcast-metrics.reducer';
+import { PodcastMetricsReducer, selectAllPodcastMetrics, selectPodcastMetricsIds, initialState as podcastMetricsInitialState, selectPodcastMetricsEntities } from './podcast-metrics.reducer';
 import { podcast } from '../../../testing/downloads.fixtures';
 import { ChartTogglePodcastAction } from '../actions';
 
@@ -13,6 +13,7 @@ describe('PodcastMetricsReducer', () => {
     interval: INTERVAL_DAILY
   };
   const metricsPropertyName = getMetricsProperty(routerParams.interval, routerParams.metricsType);
+
   beforeEach(() => {
     initialState = PodcastMetricsReducer(podcastMetricsInitialState,
       new CastlePodcastMetricsSuccessAction({
@@ -23,6 +24,65 @@ describe('PodcastMetricsReducer', () => {
     );
   });
 
+  it('should set loading status for new loading entities', () => {
+    const id = '1337';
+    const loadActionPayload = {
+      id,
+      metricsType: routerParams.metricsType,
+      interval: routerParams.interval,
+      beginDate: routerParams.beginDate,
+      endDate: routerParams.endDate
+    }
+    let newState = PodcastMetricsReducer(initialState,
+      new CastlePodcastMetricsLoadAction(loadActionPayload));
+
+    const allPodcastMetrics = selectAllPodcastMetrics(newState);
+    const loadingEntity = selectPodcastMetricsEntities(newState)[id];
+    expect(allPodcastMetrics.length).toEqual(2);
+    expect(Object.keys(loadingEntity)).not.toContain('charted');
+    expect(loadingEntity.error).toBe(null);
+    expect(loadingEntity.loading).toEqual(true);
+    expect(loadingEntity.loaded).toEqual(false);
+    expect(loadingEntity.id).toEqual(id);
+  });
+
+  it('should set loading and charted status for existing loading entities', () => {
+    const id = <string>selectPodcastMetricsIds(initialState)[0];
+    const loadActionPayload = {
+      id,
+      metricsType: routerParams.metricsType,
+      interval: routerParams.interval,
+      beginDate: routerParams.beginDate,
+      endDate: routerParams.endDate
+    }
+    let newState = PodcastMetricsReducer(initialState,
+      new CastlePodcastMetricsLoadAction(loadActionPayload));
+
+    const allPodcastMetrics = selectAllPodcastMetrics(newState);
+    const loadingEntity = selectPodcastMetricsEntities(newState)[id];
+    expect(allPodcastMetrics.length).toEqual(1);
+    expect(loadingEntity.charted).toEqual(true);
+    expect(loadingEntity.error).toBe(null);
+    expect(loadingEntity.loading).toEqual(true);
+    expect(loadingEntity.loaded).toEqual(false);
+    expect(loadingEntity.id).toEqual(id);
+  });
+
+  it('should appropriately handle failure', () => {
+    const id = '404';
+    const error = 'There was a problem';
+    let newState = PodcastMetricsReducer(initialState,
+      new CastlePodcastMetricsFailureAction({ id, error }));
+
+    const allPodcastMetrics = selectAllPodcastMetrics(newState);
+    const failedEntity = selectPodcastMetricsEntities(newState)[id];
+    expect(allPodcastMetrics.length).toEqual(2);
+    expect(failedEntity.loading).toEqual(false);
+    expect(failedEntity.loaded).toEqual(true);
+    expect(failedEntity.error).toEqual(error);
+    expect(failedEntity.id).toEqual(id);
+  })
+
   it('should update with new podcast metrics', () => {
     const allPodcastMetrics = selectAllPodcastMetrics(initialState)
     expect(allPodcastMetrics.length).toEqual(1);
@@ -30,9 +90,10 @@ describe('PodcastMetricsReducer', () => {
   });
 
   it('should update existing podcast metrics keyed by id', () => {
+    const id = <string>selectPodcastMetricsIds(initialState)[0];
     let newState = PodcastMetricsReducer(initialState,
       new CastlePodcastMetricsSuccessAction({
-        id: <string>selectPodcastMetricsIds(initialState)[0],
+        id,
         metricsPropertyName,
         metrics: [
           ['2017-08-27T00:00:00Z', 52522],
@@ -50,9 +111,11 @@ describe('PodcastMetricsReducer', () => {
         ]
       })
     );
-    const allPodcastMetrics = selectAllPodcastMetrics(newState)
+    const allPodcastMetrics = selectAllPodcastMetrics(newState);
     expect(allPodcastMetrics.length).toEqual(1);
-    expect(allPodcastMetrics[0].id).toEqual(<string>selectPodcastMetricsIds(initialState)[0]);
+    expect(allPodcastMetrics[0].loading).toEqual(false);
+    expect(allPodcastMetrics[0].loaded).toEqual(true);
+    expect(allPodcastMetrics[0].id).toEqual(id);
     expect(allPodcastMetrics[0][metricsPropertyName].length).toEqual(12);
     expect(allPodcastMetrics[0][metricsPropertyName][0][1]).toEqual(52522);
   });
--- src/app/ngrx/reducers/podcast-metrics.reducer.ts
+++ src/app/ngrx/reducers/podcast-metrics.reducer.ts
@@ -3,12 +3,7 @@ import { ActionTypes, AllActions } from '../actions'
 import { PodcastMetrics } from './models/podcast-metrics.model';
 import { UpdateStr } from '@ngrx/entity/src/models';
 
-export interface PodcastMetricsState extends EntityState<PodcastMetrics> {
-
-}
-const podcastIndex = (state: PodcastMetrics[], id: string) => {
-  return state.findIndex(p => p.id === id);
-};
+export interface PodcastMetricsState extends EntityState<PodcastMetrics>{};
 
 export const adapter: EntityAdapter<PodcastMetrics> = createEntityAdapter<PodcastMetrics>(); 
 
@@ -18,46 +13,49 @@ export function PodcastMetricsReducer(state: PodcastMetricsState = initialState,
   switch (action.type) {
     case ActionTypes.CASTLE_PODCAST_METRICS_LOAD: {
       const { id } = action.payload;
-      const podcastIdx = podcastIndex(state, id);
-      let podcast: PodcastMetrics, newState: PodcastMetrics[];
-      if (podcastIdx > -1) {
-        podcast = {...state[podcastIdx], error: null, loading: true, loaded: false};
-        newState = [...state.slice(0, podcastIdx), podcast, ...state.slice(podcastIdx + 1)];
-      } else {
-        podcast = {id, charted: true, error: null, loading: true, loaded: false};
-        newState = [podcast, ...state];
+      const currentIds = <string[]>selectPodcastMetricsIds(state);
+      const charted = currentIds.includes(id);
+      const podcastUpdate: UpdateStr<PodcastMetrics> = {
+        id,
+        changes: {
+          error: null,
+          loading: true,
+          loaded: false,
+          ...(charted && { charted }) // Conditionally add charted as object member
+        }
       }
-      return newState;
+      return adapter.upsertOne(podcastUpdate, state);
     }
     case ActionTypes.CASTLE_PODCAST_METRICS_SUCCESS: {
-
       const { id, metricsPropertyName, metrics } = action.payload;
       const podcastUpdate: UpdateStr<PodcastMetrics> = {
-        id: id,
+        id,
         changes: {
-          [metricsPropertyName]: metrics
+          [metricsPropertyName]: metrics,
+          loading: false,
+          loaded: true
         }
       }
       return adapter.upsertOne(podcastUpdate, state);
     }
     case ActionTypes.CASTLE_PODCAST_METRICS_FAILURE: {
       const { id, error } = action.payload;
-      const podcastIdx = podcastIndex(state, id);
-      let podcast: PodcastMetrics, newState: PodcastMetrics[];
-      if (podcastIdx > -1) {
-        podcast = {...state[podcastIdx], error, loading: false, loaded: false};
-        newState = [...state.slice(0, podcastIdx), podcast, ...state.slice(podcastIdx + 1)];
-      } else {
-        podcast = {id, error, loading: false, loaded: false};
-        newState = [podcast, ...state];
+      const podcastUpdate: UpdateStr<PodcastMetrics> = {
+        id,
+        changes: {
+          error,
+          loading: false,
+          loaded: true
+        }
       }
-      return newState;
+      return adapter.upsertOne(podcastUpdate, state);
     }
     case ActionTypes.CHART_TOGGLE_PODCAST: {
+      const { id, charted } = action.payload;
       const podcastUpdate: UpdateStr<PodcastMetrics> = {
-        id: action.payload.id,
+        id,
         changes: {
-          charted: action.payload.charted
+          charted
         }
       }
       return adapter.updateOne(podcastUpdate, state);

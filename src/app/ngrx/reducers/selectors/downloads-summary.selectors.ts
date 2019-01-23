f77a6b1c423cb957bb874f16ed732f28fd601204
Sandi Barr <sandi.barr@prx.org>
2019-01-23 16:50:08 -0600

    show aggregated episode total downloads

--- src/app/ngrx/reducers/selectors/downloads-summary.selectors.ts
+++ src/app/ngrx/reducers/selectors/downloads-summary.selectors.ts
@@ -3,17 +3,23 @@ import { selectMetricsTypeRoute } from './router.selectors';
 import { selectPodcastDownloadsFilteredTotal } from './podcast-downloads.selectors';
 import { selectRoutedPodcastRanksTotalDownloads } from './podcast-ranks.selectors';
 import { MetricsType, METRICSTYPE_DOWNLOADS } from '../models';
+import { selectEpisodeSelectedEpisodeGuids } from './episode-select.selectors';
+import { selectSelectedEpisodesRanksTotalDownloads } from './episode-ranks.selectors';
 
 export const selectDownloadsSummaryTotal = createSelector(
   selectMetricsTypeRoute,
   selectPodcastDownloadsFilteredTotal,
   selectRoutedPodcastRanksTotalDownloads,
+  selectEpisodeSelectedEpisodeGuids,
+  selectSelectedEpisodesRanksTotalDownloads,
   (metricsType: MetricsType,
    downloadsTotal: number,
-   ranksTotal: number) => {
+   podcastRanksTotal: number,
+   guids,
+   episodesRanksTotal) => {
     if (metricsType === METRICSTYPE_DOWNLOADS) {
       return downloadsTotal;
     } else {
-      return ranksTotal;
+      return guids && guids.length ? episodesRanksTotal : podcastRanksTotal;
     }
   });

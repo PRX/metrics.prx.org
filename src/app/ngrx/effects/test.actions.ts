4decae7aa485f546b555845f27af67a519bf51c4
Sandi Barr <sandi.barr@prx.org>
2019-02-28 09:17:44 -0600

    ignore test coverage for fake test actions

--- src/app/ngrx/effects/test.actions.ts
+++ src/app/ngrx/effects/test.actions.ts
@@ -2,6 +2,7 @@ import { Actions } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
 import { Observable, EMPTY } from 'rxjs';
 
+/* instanbul ignore next: fake actions for tests */
 export class TestActions extends Actions {
   constructor(source?: Observable<Action>) {
     super(EMPTY);
@@ -11,7 +12,7 @@ export class TestActions extends Actions {
     this.source = source;
   }
 }
-
+/* instanbul ignore next: fake actions for tests */
 export function getActions() {
   return new TestActions();
 }

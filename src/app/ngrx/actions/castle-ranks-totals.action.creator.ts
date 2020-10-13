856f5d24f8485346680deeeb722a4395daad7389
Sandi Barr <sandi.barr@prx.org>
2020-10-13 13:30:39 -0500

    remove old actions

--- src/app/ngrx/actions/castle-ranks-totals.action.creator.ts
+++ src/app/ngrx/actions/castle-ranks-totals.action.creator.ts
@@ -1,43 +1,11 @@
-import { Action, createAction, props } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
 import { ActionTypes } from './action.types';
 import { IntervalModel, Rank } from '../';
-
-// export interface CastlePodcastRanksLoadPayload {
-//   podcastId: string;
-//   group: string;
-//   filter?: string;
-//   interval: IntervalModel;
-//   beginDate: Date;
-//   endDate: Date;
-// }
-
-// export class CastlePodcastRanksLoadAction implements Action {
-//   readonly type = ActionTypes.CASTLE_PODCAST_RANKS_LOAD;
-
-//   constructor(public payload: CastlePodcastRanksLoadPayload) {}
-// }
 
 export const CastlePodcastRanksLoad = createAction(
   ActionTypes.CASTLE_PODCAST_RANKS_LOAD,
   props<{ podcastId: string; group: string; filter?: string; interval: IntervalModel; beginDate: Date; endDate: Date }>()
 );
-
-// export interface CastlePodcastRanksSuccessPayload {
-//   podcastId: string;
-//   group: string;
-//   filter?: string;
-//   interval: IntervalModel;
-//   beginDate: Date;
-//   endDate: Date;
-//   downloads: any[][];
-//   ranks: Rank[];
-// }
-
-// export class CastlePodcastRanksSuccessAction implements Action {
-//   readonly type = ActionTypes.CASTLE_PODCAST_RANKS_SUCCESS;
-
-//   constructor(public payload: CastlePodcastRanksSuccessPayload) {}
-// }
 
 export const CastlePodcastRanksSuccess = createAction(
   ActionTypes.CASTLE_PODCAST_RANKS_SUCCESS,
@@ -53,121 +21,30 @@ export const CastlePodcastRanksSuccess = createAction(
   }>()
 );
 
-// export interface CastlePodcastRanksFailurePayload {
-//   podcastId: string;
-//   group: string;
-//   filter?: string;
-//   interval: IntervalModel;
-//   beginDate: Date;
-//   endDate: Date;
-//   error: any;
-// }
-
-// export class CastlePodcastRanksFailureAction implements Action {
-//   readonly type = ActionTypes.CASTLE_PODCAST_RANKS_FAILURE;
-
-//   constructor(public payload: CastlePodcastRanksFailurePayload) {}
-// }
-
 export const CastlePodcastRanksFailure = createAction(
   ActionTypes.CASTLE_PODCAST_RANKS_FAILURE,
   props<{ podcastId: string; group: string; filter?: string; interval: IntervalModel; beginDate: Date; endDate: Date; error: any }>()
 );
-
-// export interface CastlePodcastTotalsLoadPayload {
-//   podcastId: string;
-//   group: string;
-//   filter?: string;
-//   beginDate: Date;
-//   endDate: Date;
-// }
-
-// export class CastlePodcastTotalsLoadAction implements Action {
-//   readonly type = ActionTypes.CASTLE_PODCAST_TOTALS_LOAD;
-
-//   constructor(public payload: CastlePodcastTotalsLoadPayload) {}
-// }
 
 export const CastlePodcastTotalsLoad = createAction(
   ActionTypes.CASTLE_PODCAST_TOTALS_LOAD,
   props<{ podcastId: string; group: string; filter?: string; beginDate: Date; endDate: Date }>()
 );
 
-// export interface CastlePodcastTotalsSuccessPayload {
-//   podcastId: string;
-//   group: string;
-//   filter?: string;
-//   beginDate: Date;
-//   endDate: Date;
-//   ranks: Rank[];
-// }
-
-// export class CastlePodcastTotalsSuccessAction implements Action {
-//   readonly type = ActionTypes.CASTLE_PODCAST_TOTALS_SUCCESS;
-
-//   constructor(public payload: CastlePodcastTotalsSuccessPayload) {}
-// }
-
 export const CastlePodcastTotalsSuccess = createAction(
   ActionTypes.CASTLE_PODCAST_TOTALS_SUCCESS,
   props<{ podcastId: string; group: string; filter?: string; beginDate: Date; endDate: Date; ranks: Rank[] }>()
 );
 
-// export interface CastlePodcastTotalsFailurePayload {
-//   podcastId: string;
-//   group: string;
-//   filter?: string;
-//   beginDate: Date;
-//   endDate: Date;
-//   error: any;
-// }
-
-// export class CastlePodcastTotalsFailureAction implements Action {
-//   readonly type = ActionTypes.CASTLE_PODCAST_TOTALS_FAILURE;
-
-//   constructor(public payload: CastlePodcastTotalsFailurePayload) {}
-// }
-
 export const CastlePodcastTotalsFailure = createAction(
   ActionTypes.CASTLE_PODCAST_TOTALS_FAILURE,
   props<{ podcastId: string; group: string; filter?: string; beginDate: Date; endDate: Date; error: any }>()
 );
-// export interface CastleEpisodeRanksLoadPayload {
-//   guid: string;
-//   group: string;
-//   filter?: string;
-//   interval: IntervalModel;
-//   beginDate: Date;
-//   endDate: Date;
-// }
-
-// export class CastleEpisodeRanksLoadAction implements Action {
-//   readonly type = ActionTypes.CASTLE_EPISODE_RANKS_LOAD;
-
-//   constructor(public payload: CastleEpisodeRanksLoadPayload) {}
-// }
 
 export const CastleEpisodeRanksLoad = createAction(
   ActionTypes.CASTLE_EPISODE_RANKS_LOAD,
   props<{ guid: string; group: string; filter?: string; interval: IntervalModel; beginDate: Date; endDate: Date }>()
 );
-
-// export interface CastleEpisodeRanksSuccessPayload {
-//   guid: string;
-//   group: string;
-//   filter?: string;
-//   interval: IntervalModel;
-//   beginDate: Date;
-//   endDate: Date;
-//   downloads: any[][];
-//   ranks: Rank[];
-// }
-
-// export class CastleEpisodeRanksSuccessAction implements Action {
-//   readonly type = ActionTypes.CASTLE_EPISODE_RANKS_SUCCESS;
-
-//   constructor(public payload: CastleEpisodeRanksSuccessPayload) {}
-// }
 
 export const CastleEpisodeRanksSuccess = createAction(
   ActionTypes.CASTLE_EPISODE_RANKS_SUCCESS,
@@ -183,80 +60,20 @@ export const CastleEpisodeRanksSuccess = createAction(
   }>()
 );
 
-// export interface CastleEpisodeRanksFailurePayload {
-//   guid: string;
-//   group: string;
-//   filter?: string;
-//   interval: IntervalModel;
-//   beginDate: Date;
-//   endDate: Date;
-//   error: any;
-// }
-
-// export class CastleEpisodeRanksFailureAction implements Action {
-//   readonly type = ActionTypes.CASTLE_EPISODE_RANKS_FAILURE;
-
-//   constructor(public payload: CastleEpisodeRanksFailurePayload) {}
-// }
-
 export const CastleEpisodeRanksFailure = createAction(
   ActionTypes.CASTLE_EPISODE_RANKS_FAILURE,
   props<{ guid: string; group: string; filter?: string; interval: IntervalModel; beginDate: Date; endDate: Date; error: any }>()
 );
-
-// export interface CastleEpisodeTotalsLoadPayload {
-//   guid: string;
-//   group: string;
-//   filter?: string;
-//   beginDate: Date;
-//   endDate: Date;
-// }
-
-// export class CastleEpisodeTotalsLoadAction implements Action {
-//   readonly type = ActionTypes.CASTLE_EPISODE_TOTALS_LOAD;
-
-//   constructor(public payload: CastleEpisodeTotalsLoadPayload) {}
-// }
 
 export const CastleEpisodeTotalsLoad = createAction(
   ActionTypes.CASTLE_EPISODE_TOTALS_LOAD,
   props<{ guid: string; group: string; filter?: string; beginDate: Date; endDate: Date }>()
 );
 
-// export interface CastleEpisodeTotalsSuccessPayload {
-//   guid: string;
-//   group: string;
-//   filter?: string;
-//   beginDate: Date;
-//   endDate: Date;
-//   ranks: Rank[];
-// }
-
-// export class CastleEpisodeTotalsSuccessAction implements Action {
-//   readonly type = ActionTypes.CASTLE_EPISODE_TOTALS_SUCCESS;
-
-//   constructor(public payload: CastleEpisodeTotalsSuccessPayload) {}
-// }
-
 export const CastleEpisodeTotalsSuccess = createAction(
   ActionTypes.CASTLE_EPISODE_TOTALS_SUCCESS,
   props<{ guid: string; group: string; filter?: string; beginDate: Date; endDate: Date; ranks: Rank[] }>()
 );
-
-// export interface CastleEpisodeTotalsFailurePayload {
-//   guid: string;
-//   group: string;
-//   filter?: string;
-//   beginDate: Date;
-//   endDate: Date;
-//   error: any;
-// }
-
-// export class CastleEpisodeTotalsFailureAction implements Action {
-//   readonly type = ActionTypes.CASTLE_EPISODE_TOTALS_FAILURE;
-
-//   constructor(public payload: CastleEpisodeTotalsFailurePayload) {}
-// }
 
 export const CastleEpisodeTotalsFailure = createAction(
   ActionTypes.CASTLE_EPISODE_TOTALS_FAILURE,
--- src/app/ngrx/actions/id.action.creator.ts
+++ src/app/ngrx/actions/id.action.creator.ts
@@ -2,28 +2,8 @@ import { createAction, props } from '@ngrx/store';
 import { ActionTypes } from './action.types';
 import { User } from '..';
 
-// export class IdUserinfoLoadAction implements Action {
-//   readonly type = ActionTypes.ID_USERINFO_LOAD;
-
-//   constructor(public payload = {}) {}
-// }
-
 export const IdUserinfoLoad = createAction(ActionTypes.ID_USERINFO_LOAD);
 
-// export interface IdUserinfoSuccessPayload {
-//   user: User;
-// }
-
-// export class IdUserinfoSuccessAction implements Action {
-//   readonly type = <string>ActionTypes.ID_USERINFO_SUCCESS;
-//   constructor(public payload: IdUserinfoSuccessPayload) {}
-// }
-
 export const IdUserinfoSuccess = createAction(ActionTypes.ID_USERINFO_SUCCESS, props<{ user: User }>());
-
-// export class IdUserinfoFailureAction implements Action {
-//   readonly type = ActionTypes.ID_USERINFO_FAILURE;
-//   constructor(public payload: any) {}
-// }
 
 export const IdUserinfoFailure = createAction(ActionTypes.ID_USERINFO_FAILURE, props<{ error }>());
